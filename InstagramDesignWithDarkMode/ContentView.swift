81e922428642dbc2ae84ef0c6e6b0bc72f697261
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-17 17:08:39 +0300

    init

--- InstagramDesignWithDarkMode/ContentView.swift
+++ InstagramDesignWithDarkMode/ContentView.swift
@@ -19,3 +19,27 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct HomePage: View {
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(0..<4) { i in
+                            VStack {
+                                Image("\(i)")
+                                    .resizable()
+                                    .frame(width: 80, height: 80)
+                                    .overlay(Circle().stroke(lineWidth: 3).fill(Color.red))
+                                    .padding(6)
+                                
+                                    Text("User")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
