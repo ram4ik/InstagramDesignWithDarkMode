f16e024617df35b9c9a7e762ac28c75940cbef15
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-17 17:34:27 +0300

    basic project

--- InstagramDesignWithDarkMode/ContentView.swift
+++ InstagramDesignWithDarkMode/ContentView.swift
@@ -10,7 +10,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        TabView {
+            NavigationView {
+                HomePage().navigationBarTitle("Instagram")
+                .navigationBarItems(leading:
+                    Button(action: {
+                        
+                    }, label: {
+                        Image("cam").resizable().frame(width: 35, height: 35)
+                    }), trailing:
+                    HStack {
+                        Button(action: {
+                            
+                        }, label: {
+                            Image("IGTV").resizable().frame(width: 35, height: 35)
+                        })
+                        Button(action: {
+                            
+                        }, label: {
+                            Image("send").resizable().frame(width: 35, height: 35)
+                        })
+                    }
+                )
+                
+            }.tabItem {
+                Image("Home")
+            }
+            
+            Text("Find").tabItem {
+                Image("find")
+            }
+            Text("add").tabItem {
+                Image("plus")
+            }
+            Text("likes").tabItem {
+                Image("heart")
+            }
+            Text("bio").tabItem {
+                Image("person")
+            }
+        }
     }
 }
 
@@ -39,7 +78,36 @@ struct HomePage: View {
                         }
                     }
                 }
+                ForEach(0..<5) { i in
+                    Feeds()
+                }
             }
+        }
+    }
+}
+
+struct Feeds: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Image("1").resizable().frame(width: 30, height: 30).clipShape(Circle())
+                Text("User").fontWeight(.light)
+                Spacer()
+                Button(action: {
+                    
+                }) {
+                    Image("menu").resizable().frame(width: 25, height: 25).foregroundColor(.white)
+                }
+            }
+            
+            Image("p1").resizable().frame(height: 300).padding([.top, .bottom], 8)
+            
+            HStack {
+                Image("comment").resizable().frame(width: 32, height: 32)
+                Image("send").resizable().frame(width: 32, height: 32)
+                Spacer()
+                Image("saved").resizable().frame(width: 32, height: 32)
+            }.padding(8)
         }
     }
 }
